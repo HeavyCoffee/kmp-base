4eccd001041b78fc610dd40bd3ee699c35d12e02
Vladimir Anisimov <heavycoffe@gmail.com>
2025-11-24 18:19:11 +0700

    Add autoinclude projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import kotlin.text.replace
+
 rootProject.name = "KmpBase"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
@@ -28,5 +30,43 @@ dependencyResolutionManagement {
     }
 }
 
-include(":app")
+internal fun Settings.includeProjects(
+    ignoreDirs: Set<String>
+) {
+    val excludePatterns = listOf("^[.]".toRegex())
+
+    fun isBuildGradleFile(file: File): Boolean {
+        return file.name == "build.gradle.kts"
+    }
+
+    fun isAvailableDir(file: File): Boolean {
+        return !ignoreDirs.contains(file.name) && file != rootDir
+    }
+
+    fun getProjectPath(projectDir: File): String {
+        return ":" + projectDir
+            .relativeTo(rootDir)
+            .invariantSeparatorsPath
+            .replace('/', ':')
+    }
+
+    rootDir
+        .walk()
+        .maxDepth(3)
+        .filter { file -> isBuildGradleFile(file) }
+        .map { it.parentFile }
+        .filter { dir ->
+            excludePatterns.any { pattern ->
+                !pattern.containsMatchIn(dir.name)
+            } && isAvailableDir(dir)
+        }
+        .forEach { dir ->
+            include(
+                getProjectPath(dir)
+            )
+        }
+}
+
+includeProjects(setOf("build-logic", "iosApp", "build", "gradle"))
+
 includeBuild("build-logic")
